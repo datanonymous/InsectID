c9e529e876b945570874d081fc8d9868f4ef86fc
Alex Ko <ko.e.alexander@gmail.com>
2019-03-05 19:57:16 -0500

    added FAB to frag 1

--- app/src/main/java/ko/alex/insectid/Tab1Fragment.java
+++ app/src/main/java/ko/alex/insectid/Tab1Fragment.java
@@ -2,12 +2,14 @@ package ko.alex.insectid;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,12 +42,18 @@ public class Tab1Fragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));//Tab1Fragment.this inside LinearLayoutManager
 
         deviceList = new ArrayList<>();
-        for(int index = 0; index < 20; index++){
+        for(int index = 0; index < 11; index++){
             Device myDevice = new Device("Device #: " + index, "Location #: " + index, "Date: " + index);
             deviceList.add(myDevice);
         }
         recyclerViewAdapter = new MyRecyclerViewAdapter(deviceList, getActivity());
         recyclerView.setAdapter(recyclerViewAdapter);
+
+        FloatingActionButton addDeviceFab = view.findViewById(R.id.addDeviceFab);
+        addDeviceFab.setOnClickListener((View v) ->{
+            //https://stackoverflow.com/questions/30752547/listener-can-be-replaced-with-lambda
+            Toast.makeText(getActivity(), "Add Device FAB clicked", Toast.LENGTH_SHORT).show();
+        });
 
         return view;
     }
